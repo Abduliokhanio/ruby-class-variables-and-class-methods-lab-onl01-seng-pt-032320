0fc85a2d3e54d6e7ec6e9ead19618c694b1d08d5
Abdul Khan <akhan17@outlook.com>
2020-04-04 21:12:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,13 @@ class Song
   attr_reader :name, :artist, :genre
   
   @@count = 0
-
+@@artist = []
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     
-    @@artist = []
+    
     @@artist << @artist
     
     @@count +=1
